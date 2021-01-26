f1919b83d35fc15ad5102af2521461fd72affcc6
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2021-01-26 15:17:27 -0500

    started movie in HelloMain.java

--- src/main/java/HelloMain.java
+++ src/main/java/HelloMain.java
@@ -1,6 +1,8 @@
 public class HelloMain {
     //test test
     public static void main(String[] args) {
-
+        System.out.println("Hello World, B21");
+        System.out.println("B21 future SDET learning GIT");
+        System.out.println("Started the movie");
     }
 }
