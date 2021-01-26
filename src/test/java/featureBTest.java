79eb5e03e234f3ad6bba6d2b6555bfe38f53ee3e
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2021-01-26 14:34:37 -0500

    Added a second SOUT to featureBTest.java

--- src/test/java/featureBTest.java
+++ src/test/java/featureBTest.java
@@ -2,6 +2,6 @@ public class featureBTest {
     public static void main(String[] args) {
 
         System.out.println("Feature B first commit");
-
+        System.out.println("Second commit homie");
     }
 }
