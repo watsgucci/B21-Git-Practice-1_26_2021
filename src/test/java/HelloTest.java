7812f0dfd30da4c1ff5e0c3cfd023b669f114e2f
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2021-01-26 13:44:28 -0500

    added 2 sout statements

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,7 @@
 public class HelloTest {
     //test test
     public static void main(String[] args) {
-
+        System.out.println("Hello cybertek");
+        System.out.println("Whats going on bruv");
     }
 }
